dee1cdabbe7fb7adc5adef1dfe0e9299d96e34eb
Neil Thomas <neilthomas@berkeley.edu>
2025-09-18 14:46:00 -0700

    Update README

---
+++ .example.environment.js
@@ -0,0 +1,14 @@
+export const ENV = {
+  URL: "https://github.com/login/oauth/authorize",
+  ACCESS_TOKEN_URL: "https://github.com/login/oauth/access_token",
+  REDIRECT_URL: " https://github.com/",
+  REPOSITORY_URL: "https://api.github.com/repos/",
+  USER_INFO_URL: "https://api.github.com/user",
+  CLIENT_SECRET: "YOUR_CLIENT_SECRET_KEY",
+  CLIENT_ID: "YOUR_CLIENT_ID",
+  SCOPES: ["repo"],
+  HEADER: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+};
